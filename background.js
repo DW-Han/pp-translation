216e980cf4c8d2cad19faac253df6227b061bce9
DW-Han <100533592+DW-Han@users.noreply.github.com>
2023-02-03 20:23:15 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,17 +1,31 @@
 chrome.downloads.onChanged.addListener(function(downloadDelta) {
     if (downloadDelta.state && downloadDelta.state.current === 'complete') {
 
-      chrome.windows.create({
-        url: "popup.html",
-        type: "popup",
-        width: 500,
-        height: 500
-      });
-
-
+      // getting last download and checking if its a power point
       chrome.downloads.search({id: downloadDelta.id}, function(downloadItems) {
         console.log(downloadItems[0].url);
+        if (downloadItems[0].filename.endsWith('.pptx')) {
+          console.log(downloadItems[0].filename + ' is a PowerPoint presentation.');
+
+          chrome.windows.create({
+            url: "popup.html",
+            type: "popup",
+            width: 500,
+            height: 500
+          });
+
+        } else {
+          console.log(downloadItems[0].filename + ' is not a PowerPoint presentation.');
+        }
       });
+
+      chrome.downloads.search({}, function(downloadItems) {
+        downloadItems.forEach(function(downloadItem) {
+          
+        });
+      });
+
+      
     }
   });
 
