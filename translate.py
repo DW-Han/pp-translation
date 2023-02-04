800f264893e14d5fee4d3d62d7c541ba5adcd5bd
DW-Han <100533592+DW-Han@users.noreply.github.com>
2023-02-04 13:23:47 -0500

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,7 +1,5 @@
 import json
 from flask import Flask, render_template, url_for, request, redirect, session, jsonify
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -9,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def default(): 
-    return redirect(url_for("translate"))
+    return render_template("setting.html")
  
 @app.route("/translate/") 
 def translate(): 
