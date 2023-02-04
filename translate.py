0d030fb0bceec207240e74cebfad16b505b79f31
charan776 <79649990+charan776@users.noreply.github.com>
2023-02-03 21:57:52 -0500

    Create translate.py

---
+++ translate.py
@@ -0,0 +1,22 @@
+import json
+from flask import Flask, render_template, url_for, request, redirect, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+
+@app.route("/") 
+def default(): 
+    return redirect(url_for("translate"))
+ 
+@app.route("/translate/", methods=["GET", "POST"]) 
+def translate(): 
+    return render_template("settings.html")
+
+
+  
+
+if __name__ == "__main__":
+    app.run(debug=True)
